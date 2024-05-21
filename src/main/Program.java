78cf78d7e8bea331d4fb30b4331ad09b43ac425c
Pedro Sperendio <Pedro.sperendio@outlook.com>
2024-05-21 15:38:22 -0300

    Reviewing intial concepts

---
+++ src/main/Program.java
@@ -0,0 +1,12 @@
+package main;
+
+
+import boardgame.Board;
+import chess.ChessMatch;
+
+public class Program {
+	public static void main(String[] args) {
+		ChessMatch chessMatch = new ChessMatch(); 
+		UI.printBoard(chessMatch.getPieces());
+	}
+}
